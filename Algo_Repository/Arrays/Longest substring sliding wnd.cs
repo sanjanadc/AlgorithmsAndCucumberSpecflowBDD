f1291e3cc387dd431559cc702207281fd20a5f84
Sanjana D C <sanjanadc6@gmail.com>
2022-04-05 23:29:01 -0700

    longest substring and Two sum algorithms

---
+++ Algo_Repository/Arrays/Longest substring sliding wnd.cs
@@ -0,0 +1,35 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_Repository.Arrays
+{
+    internal class Longest_substring_sliding_wnd
+    {
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            Dictionary<char, int> val = new Dictionary<char, int>();
+            int j = 0, max = 0,i=0;
+            for (; i < s.Length; i++)
+            {
+                if(val.ContainsKey(s[i]))
+                    j = Math.Max(j, val[s[i]] + 1);
+
+                val[s[i]] = i;
+                max = Math.Max(max, i - j + 1);
+                
+            }
+            return max;
+        }
+        static void Main()
+        {
+            string s = "abcabcbb";
+            Console.WriteLine("The input string is " + s);
+            int length = LengthOfLongestSubstring(s);
+            Console.WriteLine("The length of the longest non-repeating character substring is " + length);
+        }
+    }
+}
---
+++ Algo_Repository/Arrays/TwoSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_Repository.Arrays
+{
+    internal class TwoSum
+    {
+
+        public static int[] TwoSums(int[] array, int target)
+        {
+            Dictionary<int, int> pair = new Dictionary<int, int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int complement = target - array[i];
+
+                if (pair.ContainsKey(complement))
+                {
+                    return new int[] { pair[complement], i };
+                }
+                pair.Add(complement, i);
+            }
+            return null;
+        }
+
+
+         //static void Main(string[] args)
+         //{
+         //   int[] array = { 2, 7, 11, 15};
+         //   int target = 9;  
+         //   int[] ans =TwoSums(array,target);
+         //   foreach(int i in ans)
+         //   {
+         //       Console.WriteLine(i);
+         //   }
+         //}
+    }
+}
+//time n space complexity O(n)
