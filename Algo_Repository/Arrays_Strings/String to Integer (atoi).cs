f7bd0562b1cb4859181c8b56ba7becc6aba51cca
Sanjana D C <sanjanadc6@gmail.com>
2022-07-20 15:22:01 -0700

    String to Int

--- Algo_Repository/Arrays_Strings/String to Integer (atoi).cs
+++ Algo_Repository/Arrays_Strings/String to Integer (atoi).cs
@@ -8,43 +8,47 @@ namespace Algo_Repository.Arrays
 {
     internal class String_to_Integer__atoi_
     {
-        public static  int stringToInteger_ATOI(string s)
+        public static  int stringToInteger_ATOI(string input)
         {
-            int result = 0;
-            int sign = 1;
-            int CurrIndex = 0;
-            int size = s.Length;
-
-            //1- if exist exclude leading white spaces
-            while (CurrIndex < size && s[CurrIndex] == ' ')
-                CurrIndex++;
-
-            // 2- now index is pointing to non space charachter
-            //      if next char is - or + then set the sign var
-            if (CurrIndex < size)
+            var sign = 1;
+            var result = 0;
+            var index = 0;
+            var n = input.Length;
+            // Discard all spaces from the beginning of the input string.
+            while (index < n && input[index] == ' ')
             {
-                if (s[CurrIndex] == '-')
-                {
-                    sign = -1;
-                    CurrIndex++;
-                }
-                else if (s[CurrIndex] == '+')
-                {
-                    sign = 1;
-                    CurrIndex++;
-                }
+                index++;
             }
-
-            while (CurrIndex < size && Char.IsDigit(s[CurrIndex]))
+            // sign = +1, if it's positive number, otherwise sign = -1.
+            if (index < n && input[index] == '+')
             {
-                if (!(s[CurrIndex] - '0' >= 0 && s[CurrIndex] - '0' <= 9))
-                    return -1;
-                //else just keep updating the result
-                result = result * 10 + s[CurrIndex] - '0';
-                CurrIndex++;
+                sign = 1;
+                index++;
             }
+            else if (index < n && input[index] == '-')
+            {
+                sign = -1;
+                index++;
+            }
+            // Traverse next digits of input and stop if it is not a digit
+            while (index < n && char.IsDigit(input[index]))
+            {
+                var digit = (input[index]) - ('0');
+                // Check overflow and underflow conditions.
+                if ((result > (int.MaxValue / 10)) || (result == (int.MaxValue / 10) && digit > int.MaxValue % 10))
+                {
+                    // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.
+                    return sign == 1 ? int.MaxValue : int.MinValue;
+                }
+                // Append current digit to the result.
+                result = 10 * result + digit;
+                index++;
+            }
+            // We have formed a valid number without any overflow/underflow.
+            // Return it after multiplying it with its sign.
+            return sign * result;
 
-                return sign * result;
+
         }
 
         // Driver code
