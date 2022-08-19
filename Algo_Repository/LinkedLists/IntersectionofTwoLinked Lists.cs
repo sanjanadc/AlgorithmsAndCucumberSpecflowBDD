cf4d9858cfdbb3b05c2d55a12c17f44bddfd167d
Sanjana D C <sanjanadc6@gmail.com>
2022-08-18 21:00:07 -0700

    Linked_lists

---
+++ Algo_Repository/LinkedLists/IntersectionofTwoLinked Lists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_Repository.LinkedLists
+{
+    internal class IntersectionofTwoLinked_Lists
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+
+            ListNode pA = headA;
+            ListNode pB = headB;
+            while (pA != pB)
+            {
+                pA = pA == null ? headB : pA.next;
+                pB = pB == null ? headA : pB.next;
+            }
+            return pA;
+
+
+
+        }
+
+    }
+}
---
+++ Algo_Repository/LinkedLists/RemoveDuplicatesfromSortedList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_Repository.LinkedLists
+{
+    internal class RemoveDuplicatesfromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode current = head;
+            while (current != null && current.next != null)
+            {
+                if (current.next.val == current.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+            return head;
+        }
+
+    }
+}
