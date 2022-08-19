246da758987b01ef4cb0033df57bc1facbb93c6d
Sanjana D C <sanjanadc6@gmail.com>
2022-08-19 11:01:22 -0700

    Linked List

---
+++ Algo_Repository/LinkedLists/PalindromeLinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_Repository.LinkedLists
+{
+    internal class PalindromeLinkedList
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+
+            // Find the end of first half and reverse second half.
+            ListNode firstHalfEnd = endOfFirstHalf(head);
+            ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+            // Check whether or not there is a palindrome.
+            ListNode p1 = head;
+            ListNode p2 = secondHalfStart;
+            bool result = true;
+            while (result && p2 != null)
+            {
+                if (p1.val != p2.val) result = false;
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+
+            // Restore the list and return the result.
+            firstHalfEnd.next = reverseList(secondHalfStart);
+            return result;
+        }
+        private ListNode reverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null)
+            {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            return prev;
+        }
+
+        private ListNode endOfFirstHalf(ListNode head)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast.next != null && fast.next.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+
+}
+
