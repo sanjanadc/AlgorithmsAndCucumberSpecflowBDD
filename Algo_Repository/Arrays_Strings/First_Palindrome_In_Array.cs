67fcfe3ab9cd13f1c7ad8c3b124f044efeab221c
Sanjana D C <sanjanadc6@gmail.com>
2022-09-29 20:43:47 -0700

    Arrays

---
+++ Algo_Repository/Arrays_Strings/First_Palindrome_In_Array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_Repository.Arrays_Strings
+{
+    internal class FirstPalindrome_array
+    {
+        
+            public static string FirstPalindrome(string[] words)
+            {
+                string str = "";
+                bool val;
+                foreach (string s in words)
+                {
+                    int i = 0;
+                    int j = s.Length - 1;
+                    val = ispalindrome(s, i, j);
+                if (val == true)
+                    return s;
+                }
+                return null;
+            }
+
+            private static bool ispalindrome(string s, int i, int j)
+            {
+                
+                while (i < j)
+                {
+                    if (s[i] != s[j])
+                    {
+                        return false;
+                    }
+                    i++;
+                    j--;
+
+                }
+
+                return true;
+            }
+
+        //public static void Main()
+        //{
+        //    string[] s = { "abc", "car", "ada", "racecar", "cool" };
+            
+            
+        //    Console.WriteLine(FirstPalindrome(s));
+        //}
+    }
+
+}
