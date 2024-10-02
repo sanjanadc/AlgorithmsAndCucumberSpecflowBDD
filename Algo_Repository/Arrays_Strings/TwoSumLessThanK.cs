5106783462ee8233409629aedd3ad96c920f5c59
Sanjana D C <sanjanadc6@gmail.com>
2024-10-01 19:48:27 -0700

    new additions to Array

---
+++ Algo_Repository/Arrays_Strings/TwoSumLessThanK.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    
+    public static int TwoSumLessThanK(int[] nums, int k)
+    {
+
+        Array.Sort(nums);
+
+        int i = 0;
+        int j = nums.Length - 1;
+        int max = -1;
+        while (i < j)
+        {
+            int sum = nums[i] + nums[j];
+            if (sum < k)
+            {
+                max = Math.Max(sum, max);
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+
+        }
+      return max;
+
+    }
+    
+
+    static void Main()
+    {
+        // example usage
+        int[] nums = new int[] { 10,20,30 };
+        TwoSumLessThanK(nums, 15);
+    }
+}
---
+++ Algo_Repository/Arrays_Strings/largest_three_distinct_elements_in_array.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_Repository.Arrays_Strings
+{
+    internal class largest_three_distinct_elements_in_array
+    {
+        public static int[] dictinct(int[] arr)
+        {
+            int i, first, second, third;
+            if(arr.Length <3)
+                return null;
+
+            first = second = third = 0;
+            for(i=0;i<arr.Length;i++)
+            {
+                if(arr[i]>first)
+                {
+                    third = second;
+                    second= first;
+                    first = arr[i];
+                }
+                else if(arr[i]>second && arr[i]!=first)
+                {
+                    third = second;
+                    second= arr[i];
+                }
+                else if(arr[i]>third && arr[i]!=second)
+                {
+                    third=arr[i];
+                }
+            }
+            return new int[] { first, second, third };
+        }
+
+        //public static void Main()
+        //{
+        //    int[] arr = { 10, 4, 3, 50, 23, 90 };
+        //    largest_three_distinct_elements_in_array.dictinct(arr);
+
+
+        //}
+    }
+
+   
+}
